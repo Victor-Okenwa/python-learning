a113f14e6b210cbe739c08bc0b64366e3479080b
Victor-Okenwa <okenwavictor003@gmail.com>
2026-08-11 12:34:59 +0100

    Refactor LLMRequest class to LLMRequest2 and introduce TokenCounter class for token management.

--- 7.classes_oop/main.py
+++ 7.classes_oop/main.py
@@ -16,11 +16,28 @@ class Config:
     model: str
     temp: float = 0.9
 config = Config("Grok", 0.8)
-print(config.model_name)
+# print(config.model_name)
 
-class LLMRequest(BaseModel):
+class LLMRequest2(BaseModel):
     model_name: str
     temp: float = Field(default = 0.7, ge = 0.0, le = 2.0)
 
-llm = LLMRequest("gpt", 0.5)
+llm = LLMRequest2(model_name="gpt", temp= 0.5)
 print(llm)
+
+
+# Exercise
+class TokenCounter:
+    def __init__(self, model_name: str, total_tokens: int = 0) -> None:
+        self.model_name: str = model_name
+        self._total_tokens: int = total_tokens
+
+    def add_tokens(self, count: int) -> None:
+        self._total_tokens += count
+
+    def get_total(self) -> int:
+        return self._total_tokens
+
+counter = TokenCounter("gpt-4o")
+counter.add_tokens(150)
+print(counter.get_total())
